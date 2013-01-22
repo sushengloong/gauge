27fba1a608a6f0791a9e4a41b98c6cc2549006f0
Sheng-Loong Su <sushengloong@gmail.com>
2013-01-22 22:36:21 +0800

    replace with actual navigation menu items

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,2 @@
 module ApplicationHelper
-  def loginout_text
-    current_user.present? ? 'Logout' : 'Login'
-  end
-
-  def loginout_path
-    current_user.present? ? logout_path : login_path
-  end
 end
