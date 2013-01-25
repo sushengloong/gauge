42af3ed8144c0e9c87a45255b87004f65c28ce05
Sheng-Loong Su <sushengloong@gmail.com>
2013-01-25 22:26:24 +0800

    add new goal modal form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
+  layout :layout
+
   private
   def current_user
     @current_user ||= User.find_by_auth_token(cookies[:auth_token]) if cookies[:auth_token]
@@ -8,6 +10,15 @@ class ApplicationController < ActionController::Base
 
   def authorize
     redirect_to login_url, alert: "Not authorized" if current_user.nil?
+  end
+
+  def layout
+    if params[:layout].blank?
+      'application'
+    else
+      layout_name = params[:layout].chomp.strip.downcase
+      layout_name == 'false' ? false : layout_name
+    end
   end
 
   helper_method :current_user
